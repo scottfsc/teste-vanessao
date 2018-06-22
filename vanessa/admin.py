a2cf265205e10ddd54502d21eb28d7bda18a95d2
gislene lima <vanessa@fiberlink.net.br>
2018-06-21 22:07:50 -0300

    home foi criada

--- vanessa/admin.py
+++ vanessa/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ('name', 'email', 'genero', 'idade')
+    
+
--- vanessa/models.py
+++ vanessa/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    nome = models.CharField(max_length = 50)
+    email = models.EmailField()
+    genero = models.CharField()
+    idade = models.IntegerField()
+
+    def __str__(self):
+        return('{}\n{}\n{}\n{}'.format(self.name, self.email, self.genero, self.idade))
+
+    
+
+    
+ 
